dc10ee8474674c3eaf73d7c85c1b3ee6a75f82fe
Caique Roschel <croschel000@gmail.com>
2021-07-06 20:50:26 -0300

    chore: adjusts on structure

--- App.tsx
+++ App.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { SafeAreaView, StatusBar, Text } from 'react-native';
 import Routes from '~/routes';
 import 'react-native-gesture-handler';
+import { colors } from '~/global/styles/theme';
 
 const App = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" backgroundColor="transparent" />
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <Routes />
     </>
   );
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { Home } from '~/screens/Home';
+import { Welcome } from '~/screens/Welcome';
 
 const Stack = createStackNavigator();
 
 export const MainRoutes = () => {
   return (
-    <Stack.Navigator headerMode="none">
+    <Stack.Navigator initialRouteName="Welcome" headerMode="none">
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Welcome" component={Welcome} />
     </Stack.Navigator>
   );
 };
