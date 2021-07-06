55217635ef853264a9b97bfee039f817cc47a7f4
Caique Roschel <croschel000@gmail.com>
2021-07-06 20:49:41 -0300

    wip: theme colors

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  primary: '#232535',
+  secondary: '#2d3340',
+  coldInitial: '#6efbe6',
+  coldEnd: '#72efee',
+  sunnyInitial: '#f9e177',
+  sunnyEnd: '#ffbe94',
+  rainInitial: '#59e4fe',
+  rainEnd: '#5accf9',
+
+  overlay: 'rgba(0,0,0,0.7)',
+  highlight: '#ABB1CC',
+  heading: '#DDE3F0',
+};
