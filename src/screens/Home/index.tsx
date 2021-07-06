c9e94f771f2e4e79992870d3ddbb197dc88f6220
Caique Roschel <croschel000@gmail.com>
2021-07-06 20:50:12 -0300

    wip: home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import { View, Text } from 'react-native';
+import { GradientBackground } from '~/components/GradientBackground';
+import { colors } from '~/global/styles/theme';
 
 import { styles } from './styles';
 
 export const Home = () => {
   return (
-    <View style={styles.container}>
-      <Text>HOME</Text>
-    </View>
+    <GradientBackground
+      firstColor={colors.coldInitial}
+      secondColor={colors.coldEnd}>
+      <View style={styles.container}>
+        <Text>HOME</Text>
+      </View>
+    </GradientBackground>
   );
 };
