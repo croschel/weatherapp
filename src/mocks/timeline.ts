14fb18ab0af48969464fe57afa83623e34ccc92f
Caique Roschel <croschel000@gmail.com>
2021-07-08 12:35:55 -0300

    wip: mocks for cards

---
+++ src/mocks/timeline.ts
@@ -0,0 +1,26 @@
+export const timeline = [
+  {
+    id: '1',
+    temp: 28,
+    hour: '9:00 AM',
+    type: 'cloud',
+  },
+  {
+    id: '2',
+    temp: 25,
+    hour: '10:00 AM',
+    type: 'cold',
+  },
+  {
+    id: '3',
+    temp: 30,
+    hour: '11:00 AM',
+    type: 'sun',
+  },
+  {
+    id: '4',
+    temp: 28,
+    hour: '12:00 AM',
+    type: 'rain',
+  },
+];
